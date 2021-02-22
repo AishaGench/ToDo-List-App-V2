b3701eb685bf6b1f594c89916d20dea40a1b7715
Aisha <aisha.gench@gmail.com>
2021-02-22 03:11:02 -0600

    Initialized the project

---
+++ script.js
@@ -0,0 +1,36 @@
+//Task 1-When a user enter the name of the item and click add button, the item will be added to the list.
+
+const itemList = localStorage.getItem('todoItems').split(',');
+
+itemList.forEach(element => {
+  const item = document.createElement('li');
+  item.innerHTML = element;
+  item.onclick = function() {
+    document.getElementById('todo_list').removeChild(item);
+  };
+  
+  document.getElementById('todo_list').appendChild(item);
+ 
+});
+
+function addItem (){
+    // alert(document.getElementById('todo_input').value);
+    if (document.getElementById('todo_input').value) {
+      const item = document.createElement('li');
+      item.innerHTML = document.getElementById('todo_input').value;
+      // item.onclick = () => removeItem(item);
+      item.onclick = function() {
+        document.getElementById('todo_list').removeChild(item);
+      };
+      document.getElementById('todo_list').appendChild(item);
+      document.getElementById("todo_input").value = "";
+      
+    } else {
+      alert('Enter an item')
+    }
+}
+
+/* //Task 2-When a user click on the item in the list, the item will be removed / deleted.
+function removeItem(item) {
+    document.getElementById('todo_list').removeChild(item);
+  } */

795b8602177529c5f07dc3094a425b8c36b0fd50
Aisha <aisha.gench@gmail.com>
2021-02-27 13:34:06 -0600

    created Local-Storege

---
+++ local-storage.js
@@ -0,0 +1,54 @@
+/* localStorage.setItem('myCat', 'Minosh'); //Setting value
+const myCat = localStorage.getItem('myCat'); //Geting value
+localStorage.removeItem('myCat'); //remove by one by
+localStorage.clear(); //removes everthing inside of local storage
+alert(myCat); */
+
+const todoItems = [];
+
+function addItem(){ //it is declera function
+    const inputBox = document.getElementById('todo_input');
+    todoItems.push(inputBox.value);
+    localStorage.setItem('todoItems', todoItems);
+    console.log(todoItems);
+    getItem()
+}
+
+function getItem() {
+    const itemsToList = localStorage.getItem('todoItems').split(',');
+    //const itemsToList = localStorage.getItem('todoItems') ?localStorage.getItem('todoItems').split(',') :[];
+    document.getElementById('todo_list').innerHTML ='';
+
+    itemsToList.forEach((item) => {
+      document.getElementById('todo_list').innerHTML += `<li>${item}</li>`;
+    })
+}
+
+getItem()
+
+
+
+/* todoItems.push('Tidy your room');
+todoItems.push('Have a breakfast');
+todoItems.push('Go to dentist');
+
+localStorage.setItem('todoItems', todoItems);
+console.log(localStorage.getItem('todoItems')) */
+
+
+
+// const myCat = 'Minosh'; //Setting value
+// alert(myCat); //Getting value
+
+/* const inputBox = document.getElementById('todo_input');
+// console.log(inputBox.id); //id yi gosterir
+// console.log(inputBox.name); // ismini gosterir
+// console.log(inputBox.type); // type gosterir
+
+console.log(inputBox) */
+
+/* function addItem() {
+    const inputBox = document.getElementById('todo_input');
+    localStorage.setItem('todoItem', inputBox.value);
+    console.log(localStorage.getItem('todoItem'))
+  } */
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // const ulList = document.querySelector('ul');
-const ulList = document.getElementById('todo_list');
+/* const ulList = document.getElementById('todo_list');
 
 function addItem() {
   const inputValue = document.getElementById('todo_input').value;
@@ -10,4 +10,41 @@ function addItem() {
     ulList.removeChild(listItem);
   }
   ulList.appendChild(listItem)
+} */
+const ulList =  document.getElementById("quote_list");
+​
+function addItem (){
+    const inputValuePerson = document.getElementById('person_name').value;
+    const inputValueText = document.getElementById('famous_quote').value;
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `${inputValuePerson} - <em>${inputValueText} </em>`
+    listItem.onclick = function () {
+                ulList.removeChild(listItem)
+                
+          }
+    ulList.appendChild(listItem)
+    
+}
+
+const addQuote = () => {
+  let inputPerson = document.getElementById("person").value;
+  let inputQuote = document.getElementById("quote").value;
+  
+  if(inputPerson && inputQuote){
+      let item = document.createElement("li");
+      item.innerHTML =`"${inputQuote}" -<em>${inputPerson}</em>`;
+​
+      item.onclick = () => {
+       removeQuote(item)  
+      };
+​
+      document.getElementById("myList").appendChild(item);
+      document.getElementById("person").value= "";
+      document.getElementById("quote").value= "";
+  } else {
+      alert("Enter a Person and Quote!")
+  }
+}
+const removeQuote = (item) => {
+  document.getElementById("myList").removeChild(item)
 }
